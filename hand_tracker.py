f0484dab3efffe6bc7982fc8aee7fc8322ecea28
Diego Pedroza <144750295+Diego5936@users.noreply.github.com>
2025-09-05 17:23:03 -0400

    Fps added

--- hand_tracker.py
+++ hand_tracker.py
@@ -1,28 +1,38 @@
 import cv2
 import mediapipe as mp
-
-handSolution = mp.solutions.hands
-hands = handSolution.Hands()
+import time
 
 # Open default camera
 videoCap = cv2.VideoCapture(0)
+lastFrameTime = 0
+
+# Hand initialization
+handSolution = mp.solutions.hands
+hands = handSolution.Hands()
 
 while True:
     # Read image
     success, img = videoCap.read()
 
-    if not success:
-        break
+    if success:
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        # Calculate fps
+        thisFrameTime = time.time()
+        fps = 1 / (thisFrameTime - lastFrameTime)
+        lastFrameTime = thisFrameTime
 
-    # Recognize hands
-    recHands = hands.process(img)
+        cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    if recHands.multi_hand_landmarks:
-        for hand in recHands.multi_hand_landmarks:
-            # Draw dots on points
-            for datapoint_id, point in enumerate(hand.landmark):
-                h, w, c = img.shape
-                x, y = int(point.x * w), int(point.y * h)
-                cv2.circle(img, (x, y), 10, (255, 0, 255), cv2.FILLED)
-    cv2.imshow("CamOutput", img)
-    cv2.waitKey(1)
+        # Recognize hands
+        recHands = hands.process(img)
+
+        if recHands.multi_hand_landmarks:
+            for hand in recHands.multi_hand_landmarks:
+                # Draw dots on points
+                for datapoint_id, point in enumerate(hand.landmark):
+                    h, w, c = img.shape
+                    x, y = int(point.x * w), int(point.y * h)
+                    cv2.circle(img, (x, y), 10, (255, 0, 255), cv2.FILLED)
+        cv2.imshow("CamOutput", img)
+        cv2.waitKey(1)
